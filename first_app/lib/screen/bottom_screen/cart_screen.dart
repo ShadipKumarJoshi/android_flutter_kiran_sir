23be6ddcd43209a15d9abe8addd714e6268481ed
Shadip Kumar Joshi <220049@softwarica.edu.np>
2024-05-13 12:28:48 +0545

    cart

--- first_app/lib/screen/bottom_screen/cart_screen.dart
+++ first_app/lib/screen/bottom_screen/cart_screen.dart
@@ -2,15 +2,49 @@
 
 import 'package:flutter/material.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
 
   @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  // manula items list
+  List<Item> lstItems = [
+
+  ];
+  
+  @override
   Widget build(BuildContext context) {
-    return const SizedBox.expand(
-      child: Center(
-        child: Text('Cart'),
-      ),
+    return SizedBox.expand(
+      child: DataTable(
+
+          // Color fill in column heading
+          headingRowColor: MaterialStateColor.resolveWith(
+            (states) => Colors.amber,
+          ),
+          border: TableBorder.all(
+            color: Colors.black,
+          ),
+          columns: const [
+            DataColumn(
+              label: Text('Item Name'),
+            ),
+            DataColumn(
+              label: Text('Item Price'),
+            ),
+          ],
+          rows: const [
+            DataRow(cells: [
+              DataCell(Text('Pencil')),
+              DataCell(Text('12')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('Pen')),
+              DataCell(Text('50')),
+            ]),
+          ]),
     );
   }
 }
